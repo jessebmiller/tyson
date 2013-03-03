11c76d25f62d2627de5c022cfa5168fb57fcb2c0
Jesse B Miller <jesse@jessebmiller.com>
2013-03-02 18:11:45 -0800

    learning git aparently

---
+++ client/tysonTemplates.js
@@ -0,0 +1,11 @@
+Handlebars.registerHelper('renderThisPage', function () {
+    return TYSON.composeFromPath(Session.get('path'));
+});
+
+Handlebars.registerHelper('renderThisContent', function () {
+    return Template[this.type](this);
+});
+
+Template.contentList.renderContent = function () {
+    return Template[this.type](this);
+}
