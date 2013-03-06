bd9b46244a0a0b3cf26896f57321b51dfd2ac2ba
Jesse B Miller <jesse@jessebmiller.com>
2013-03-06 01:09:45 -0800

    added default 404 and default homepage

--- client/urls.js
+++ client/urls.js
@@ -1,11 +1,15 @@
 var Urls = Backbone.Router.extend({
     routes: {
+        '': 'setEmptyPath',
         '*path': 'setPath',
     },
     setPath: function (path) {
         path = path.split('/');
         path.unshift('basePathHandler');
         Session.set('path', path);
+    },
+    setEmptyPath: function () {
+        Session.set('path', ['basePathHandler']);
     }
 });
 
